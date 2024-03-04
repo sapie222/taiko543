dd4c2b0546cd8daaa6b65f29aa246adac54d24e9
sapie222 <138426375+sapie222@users.noreply.github.com>
2024-03-04 21:41:53 +0100

    first commit

---
+++ pages/memes-nfts.vue
@@ -0,0 +1,42 @@
+<template>
+  <Head>
+    <Title>Memes & NFTs | {{ $config.projectMetadataTitle }}</Title>
+    <Meta property="og:title" :content="'Memes & NFTs | ' + $config.projectMetadataTitle" />
+  </Head>
+
+  <ChatFeed 
+    class="mt-1" 
+    :showQuotedPost="$config.showRepliesOnHomepage" 
+    :allPosts="true" 
+    orbisContext="kjzl6cwe1jw1465jg79owtsej54l54td3psq2m3umptns5s8zxniys108ox1bkt" 
+  />
+</template>
+
+<script>
+import { useEthers } from 'vue-dapp'
+import ChatFeed from "../components/chat/ChatFeed.vue"
+
+export default {
+  name: "memesnfts",
+
+  components: {
+    ChatFeed
+  },
+
+  computed: {
+    getOrbisContext() {
+      if (this.$config.orbisTest) {
+        return this.$config.orbisTestContext;
+      } else {
+        return this.$config.orbisContext;
+      }
+    }
+  },
+
+  setup() {
+    const { address, chainId } = useEthers()
+
+    return { address, chainId }
+  }
+}
+</script>
---
+++ pages/shill.vue
@@ -0,0 +1,42 @@
+<template>
+  <Head>
+    <Title>Shill and discuss projects | {{ $config.projectMetadataTitle }}</Title>
+    <Meta property="og:title" :content="'Shill and discuss projects | ' + $config.projectMetadataTitle" />
+  </Head>
+
+  <ChatFeed 
+    class="mt-1" 
+    :showQuotedPost="$config.showRepliesOnHomepage" 
+    :allPosts="true" 
+    orbisContext="kjzl6cwe1jw14bbfcpyha0lk1q3nrb1suoec3edma8i8b0g0lbyh73bl57igwlr" 
+  />
+</template>
+
+<script>
+import { useEthers } from 'vue-dapp'
+import ChatFeed from "../components/chat/ChatFeed.vue"
+
+export default {
+  name: "shill",
+
+  components: {
+    ChatFeed
+  },
+
+  computed: {
+    getOrbisContext() {
+      if (this.$config.orbisTest) {
+        return this.$config.orbisTestContext;
+      } else {
+        return this.$config.orbisContext;
+      }
+    }
+  },
+
+  setup() {
+    const { address, chainId } = useEthers()
+
+    return { address, chainId }
+  }
+}
+</script>
